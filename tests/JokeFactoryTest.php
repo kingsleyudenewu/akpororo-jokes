dc158950c6dde4fd40cfe4d39dd995eb5d3f5031
Udenewu Kingsley <kingzpacking@gmail.com>
2020-01-14 02:05:49 +0100

    Apply fixes from StyleCI (#1)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -1,19 +1,17 @@
 <?php
 
-
 namespace KingsleyUdenewu\AkpororoJokes\Tests;
-
 
 use KingsleyUdenewu\AkpororoJokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
 
-class JokeFactoryTest extends  TestCase
+class JokeFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_random_joke()
     {
         $jokes = new JokeFactory([
-            'This is a joke from Akpororo'
+            'This is a joke from Akpororo',
         ]);
 
         $joke = $jokes->getRandomJoke();
